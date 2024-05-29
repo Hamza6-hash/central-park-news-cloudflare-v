9ce6a924a70f7d28969d6b902e5f02cf811f781b
tayyab030 <tayyaboff564@gmail.com>
2024-05-30 01:41:29 +0500

    apply hover on the popular link.

--- components/suggestedBlogs/SuggestedBlogs.tsx
+++ components/suggestedBlogs/SuggestedBlogs.tsx
@@ -13,7 +13,7 @@ const SuggestedBlogs = () => {
     return (
         <div className="bg-primary-700 px-generic md:py-14 py-10 flex items-center justify-center">
             <div className="max-width">
-                <h1 className="font-bold text-[32px] text-primary-900 mb-2 uppercase">{isActive ? 'BLOGS' : 'Articles'}</h1>
+                <h1 className="font-bold text-[32px] text-primary-900 mb-7 uppercase">{isActive ? 'BLOGS' : 'Articles'}</h1>
                 <div className="grid grid-cols-4 md:gap-4 gap-8 max-xl:grid-cols-2 max-md:grid-cols-1">
                     {[1, 2, 3, 4,].map((item) => (<React.Fragment key={item}>
                         <BlogsCard showDateTimeInRow={true} />
