51d0acc95ebde945306daa358f35a50f5a65bd25
Arif Rahman <arifrahman1k7@gmail.com>
2025-08-05 14:58:53 +0500

    GTM UPDATED

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,17 +28,17 @@ export default function RootLayout({
           strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P8GJNKXZ');
+})(window,document,'script','dataLayer','GTM-M5W79LR8')
             `,
           }}
         />
       </head>
       <body className="select-none ">
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P8GJNKXZ"
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M5W79LR8"
           height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
         <Providers>
           <ToastProvider>
