9ed054d08284741270871f1e824e609cc5d6a7c0
Arif Rahman <arifrahman1k7@gmail.com>
2025-08-18 18:50:37 +0500

    udpated

--- components/Ads/Adbox.tsx
+++ components/Ads/Adbox.tsx
@@ -2,39 +2,49 @@ import React from "react";
 import Image from "next/image";
 
 const Adbox = () => {
+
+  const adLink = "https://www.scottbaronassociates.com/";
+
   return (
     <div className="flex justify-center items-center mt-3 w-full">
       {/* Desktop Ad */}
-      <div className="relative  hidden sm:block w-full max-w-[510px] aspect-[17/10] overflow-hidden rounded-lg">
-        <Image
-          src="/image (12).png"
-          alt="desktop ad"
-          fill
-          className="object-contain"
-          sizes="(min-width: 640px) 510px, 100vw"
-          quality={70}
-          loading="eager"
-          priority={true}
-          fetchPriority="high"
-        />
-      </div>
-
-      {/* Mobile Ad */}
-      <div className="block sm:hidden w-full">
-        <div className="relative w-full aspect-[3/4] rounded-lg overflow-hidden ">
+      <a
+        href={adLink}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="w-full"
+      >
+        <div className="relative  hidden sm:block w-full max-w-[510px] aspect-[17/10] overflow-hidden rounded-lg">
           <Image
-            src="/bottomBanner.webp"
-            alt="mobile ad"
+            src="/image (12).png"
+            alt="desktop ad"
             fill
-            quality={65}
-            className="object-contain pointer-events-none select-none"
-            sizes="(max-width: 768px) 100vw, 364px"
+            className="object-contain"
+            sizes="(min-width: 640px) 510px, 100vw"
+            quality={70}
             loading="eager"
             priority={true}
             fetchPriority="high"
           />
         </div>
-      </div>
+
+        {/* Mobile Ad */}
+        <div className="block sm:hidden w-full">
+          <div className="relative w-full aspect-[3/4] rounded-lg overflow-hidden ">
+            <Image
+              src="/bottomBanner.webp"
+              alt="mobile ad"
+              fill
+              quality={65}
+              className="object-contain pointer-events-none select-none"
+              sizes="(max-width: 768px) 100vw, 364px"
+              loading="eager"
+              priority={true}
+              fetchPriority="high"
+            />
+          </div>
+        </div>
+      </a>
     </div>
   );
 };
